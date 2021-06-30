192b17d88f48253543b02d94f646221a404fc45d
Oldman10000 <patrickoldman1993@gmail.com>
2021-06-30 14:31:23 +0000

    Add fields to resort model

--- resorts/models.py
+++ resorts/models.py
@@ -3,9 +3,22 @@ from django.db import models
 
 class Resort(models.Model):
     name = models.CharField(max_length=36)
-    description = models.TextField()
+    description = models.TextField(null=True, blank=True)
     extra_info = models.TextField(null=True, blank=True)
-    difficulty = models.CharField(max_length=12, null=True, blank=True)
+    SMALL = 'Small'
+    MEDIUM = 'Medium'
+    LARGE = 'Large'
+    size = models.CharField(max_length=6)
+    street_address1 = models.CharField(max_length=80,
+                                       null=False,
+                                       blank=False)
+    street_address2 = models.CharField(max_length=80,
+                                       null=False,
+                                       blank=False)
+    postcode = models.CharField(max_length=20, null=True, blank=True)
+    phone_number = models.CharField(max_length=20, null=False, blank=False)
+    website = models.URLField(null=True, blank=True)
+    email = models.EmailField(null=True, blank=True)
     scenic = models.BooleanField(default=False, null=True, blank=True)
     adult_price = models.DecimalField(max_digits=6,
                                       decimal_places=2,
