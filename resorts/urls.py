7c8e8eca44bbb2330cf9263c150da8ce8f4716b9
Oldman10000 <patrickoldman1993@gmail.com>
2021-06-30 14:56:37 +0000

    add resort detail view and template

--- resorts/urls.py
+++ resorts/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.all_resorts, name='resorts')
+    path('', views.all_resorts, name='resorts'),
+    path('<resort_id>', views.resort_detail, name='resort_detail'),
 ]
--- resorts/views.py
+++ resorts/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Resort
 
 
@@ -14,3 +14,17 @@ def all_resorts(request):
     }
 
     return render(request, 'resorts/resorts.html', context)
+
+
+def resort_detail(request, resort_id):
+    """
+    returns resort detail page
+    """
+
+    resort = get_object_or_404(Resort, pk=resort_id)
+
+    context = {
+        'resort': resort,
+    }
+
+    return render(request, 'resorts/resort_detail.html', context)
