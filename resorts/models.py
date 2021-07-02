4abd71c84fcd168fd74cf181549e27a28594ac53
Oldman10000 <patrickoldman1993@gmail.com>
2021-07-02 10:13:06 +0000

    Add fixtures and media for resorts

--- resorts/models.py
+++ resorts/models.py
@@ -5,21 +5,20 @@ class Resort(models.Model):
     name = models.CharField(max_length=36)
     description = models.TextField(null=True, blank=True)
     extra_info = models.TextField(null=True, blank=True)
-    SMALL = 'Small'
-    MEDIUM = 'Medium'
-    LARGE = 'Large'
     size = models.CharField(max_length=6)
-    street_address1 = models.CharField(max_length=80,
+    street_address_1 = models.CharField(max_length=80,
                                        null=False,
                                        blank=False)
-    street_address2 = models.CharField(max_length=80,
-                                       null=False,
-                                       blank=False)
+    street_address_2 = models.CharField(max_length=80,
+                                       null=True,
+                                       blank=True)
     postcode = models.CharField(max_length=20, null=True, blank=True)
-    phone_number = models.CharField(max_length=20, null=False, blank=False)
+    town_or_city = models.CharField(max_length=40, null=True, blank=True)
+    phone_number = models.CharField(max_length=20, null=True, blank=True)
     website = models.URLField(null=True, blank=True)
     email = models.EmailField(null=True, blank=True)
     scenic = models.BooleanField(default=False, null=True, blank=True)
+    family_friendly = models.BooleanField(default=False, null=True, blank=True)
     adult_price = models.DecimalField(max_digits=6,
                                       decimal_places=2,
                                       null=True,
@@ -32,15 +31,16 @@ class Resort(models.Model):
                                        decimal_places=2,
                                        null=True,
                                        blank=True)
-    x_map_reference = models.DecimalField(max_digits=4,
-                                          decimal_places=2,
+    x_map_reference = models.DecimalField(max_digits=6,
+                                          decimal_places=4,
                                           null=True,
                                           blank=True)
-    y_map_reference = models.DecimalField(max_digits=4,
-                                          decimal_places=2,
+    y_map_reference = models.DecimalField(max_digits=6,
+                                          decimal_places=4,
                                           null=True,
                                           blank=True)
     image = models.ImageField(null=True, blank=True)
+    image_credit = models.CharField(max_length=64, null=False, blank=False)
     map_image = models.ImageField(null=True, blank=True)
 
     def __str__(self):
