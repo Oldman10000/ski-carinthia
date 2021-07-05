5b2157c3e3905e5a8d9056aeec432db7037c0750
Oldman10000 <patrickoldman1993@gmail.com>
2021-07-05 13:54:15 +0000

    Add contexts and views to add ski passes to bag

---
+++ bag/contexts.py
@@ -0,0 +1,54 @@
+from django.shortcuts import get_object_or_404
+from resorts.models import Resort
+
+
+def bag_contents(request):
+
+    bag_items = []
+    total = 0
+    product_count = 0
+    bag = request.session.get('bag', {})
+
+    for item_id, adult_quantity in bag.items():
+        if adult_quantity.get('adult_quantity'):
+            product = get_object_or_404(Resort, pk=item_id)
+            total += adult_quantity.get('adult_quantity') * product.adult_price
+            product_count += adult_quantity.get('adult_quantity')
+            bag_items.append({
+                'item_id': item_id,
+                'quantity': adult_quantity.get('adult_quantity'),
+                'product': product,
+                'adult_ticket': True,
+            })
+
+    for item_id, child_quantity in bag.items():
+        if child_quantity.get('child_quantity'):
+            product = get_object_or_404(Resort, pk=item_id)
+            total += child_quantity.get('child_quantity') * product.child_price
+            product_count += child_quantity.get('child_quantity')
+            bag_items.append({
+                'item_id': item_id,
+                'quantity': child_quantity.get('child_quantity'),
+                'product': product,
+                'child_ticket': True,
+            })
+
+    for item_id, family_quantity in bag.items():
+        if family_quantity.get('family_quantity'):
+            product = get_object_or_404(Resort, pk=item_id)
+            total += family_quantity.get('family_quantity') * product.child_price
+            product_count += family_quantity.get('family_quantity')
+            bag_items.append({
+                'item_id': item_id,
+                'quantity': family_quantity.get('family_quantity'),
+                'product': product,
+                'family_ticket': True,
+            })
+
+    context = {
+        'bag_items': bag_items,
+        'total': total,
+        'product_count': product_count,
+    }
+
+    return context
--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 
 def view_bag(request):
@@ -7,3 +7,45 @@ def view_bag(request):
     """
 
     return render(request, 'bag/bag.html')
+
+
+def add_to_bag(request, item_id):
+    """
+    add items to bag
+    """
+
+    adult_quantity = int(request.POST.get('adult_quantity'))
+    child_quantity = int(request.POST.get('child_quantity'))
+    family_quantity = int(request.POST.get('family_quantity'))
+    redirect_url = request.POST.get('redirect_url')
+    bag = request.session.get('bag', {})
+
+    if adult_quantity:
+        if item_id in list(bag.keys()):
+            if 'adult_quantity' in bag:
+                bag[item_id]['adult_quantity'] += adult_quantity
+            else:
+                bag[item_id]['adult_quantity'] = adult_quantity
+        else:
+            bag[item_id] = {'adult_quantity': adult_quantity}
+
+    if child_quantity:
+        if item_id in list(bag.keys()):
+            if 'child_quantity' in bag:
+                bag[item_id]['child_quantity'] += child_quantity
+            else:
+                bag[item_id]['child_quantity'] = child_quantity
+        else:
+            bag[item_id] = {'child_quantity': child_quantity}
+
+    if family_quantity:
+        if item_id in list(bag.keys()):
+            if 'family_quantity' in bag:
+                bag[item_id]['family_quantity'] += family_quantity
+            else:
+                bag[item_id]['family_quantity'] = family_quantity
+        else:
+            bag[item_id] = {'family_quantity': family_quantity}
+
+    request.session['bag'] = bag
+    return redirect(redirect_url)
