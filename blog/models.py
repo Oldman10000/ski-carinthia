7b3dca981293fbbd39c97cd32204d568406ed56d
Oldman10000 <patrickoldman1993@gmail.com>
2021-07-12 14:42:10 +0000

    Create blog models

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+from profiles.models import UserProfile
+
+
+class Post(models.Model):
+    """
+    blog post
+    """
+    user_profile = models.ForeignKey(
+        UserProfile, on_delete=models.SET_NULL, null=True, blank=True, related_name='blogs')
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    created_date = models.DateTimeField(auto_now_add=True)
+    published_date = models.DateTimeField(blank=True, null=True, default=timezone.now)
+    views = models.IntegerField(default=0)
+    tag = models.CharField(max_length=30, blank=True, null=True)
+    image = models.ImageField(upload_to="img", blank=True, null=True)
+
+    def __str__(self):
+        return self.title
