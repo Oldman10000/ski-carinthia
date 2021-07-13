c5c783d453638b8aaccb7364c4ab57c484fe416d
Oldman10000 <patrickoldman1993@gmail.com>
2021-07-13 10:22:25 +0000

    Fix bug where sort selector did not function as expected

--- resorts/static/resorts/js/resorts.js
+++ resorts/static/resorts/js/resorts.js
@@ -1,22 +1,20 @@
 $('#sort-selector').change(function () {
-    $(document).ready(function () {
-        var selector = $(this);
-        var currentUrl = new URL(window.location);
+    var selector = $(this);
+    var currentUrl = new URL(window.location);
 
-        var selectedVal = selector.val();
-        if (selectedVal != "reset") {
-            var sort = selectedVal.split("_")[0];
-            var direction = selectedVal.split("_")[1];
+    var selectedVal = selector.val();
+    if (selectedVal != "reset") {
+        var sort = selectedVal.split("_")[0];
+        var direction = selectedVal.split("_")[1];
 
-            currentUrl.searchParams.set("sort", sort);
-            currentUrl.searchParams.set("direction", direction);
+        currentUrl.searchParams.set("sort", sort);
+        currentUrl.searchParams.set("direction", direction);
 
-            window.location.replace(currentUrl);
-        } else {
-            currentUrl.searchParams.delete("sort", sort);
-            currentUrl.searchParams.delete("direction", direction);
+        window.location.replace(currentUrl);
+    } else {
+        currentUrl.searchParams.delete("sort");
+        currentUrl.searchParams.delete("direction");
 
-            window.location.replace(currentUrl);
-        }
-    });
+        window.location.replace(currentUrl);
+    }
 });
