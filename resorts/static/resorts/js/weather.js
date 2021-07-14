2e22bfcce57960b883b1a20833655c80acfb4398
Oldman10000 <patrickoldman1993@gmail.com>
2021-07-14 14:37:52 +0000

    Improve appearance and functionality of weather feature

--- resorts/static/resorts/js/weather.js
+++ resorts/static/resorts/js/weather.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
     // weather api
 
-    const key = 'LotdkzXJjnLh5l3hbwUfEQittwSEmy0f';
+    const key = 'csf4ZQohX49VLQDoT2PBbNcLe4FxvEMS';
 
     // get weather information
     const getWeather = async (id) => {
@@ -44,41 +44,34 @@ $(document).ready(function () {
 
         days = [];
 
-        const populateDays = (days) => {
-            weather.DailyForecasts.forEach(day => {
-                days.push(day);
-            });
-        };
+        weather.DailyForecasts.forEach(day => {
+            days.push(day);
+        });
 
         console.log(days);
 
         details = document.querySelectorAll('.details');
 
-        populateDays(days);
-
         details.forEach(detail => {
             day = days[0];
             detail.innerHTML = `
             <div>
-                <p>${new Date(day.Date).toLocaleDateString('en-us', { weekday:"long", month:"short", day:"numeric"})}</p>
+                <p><strong>${new Date(day.Date).toLocaleDateString('en-us', { weekday:"long", month:"short", day:"numeric"})}</strong></p>
+            </div>
+            <div class="icon">
+                <img src="../../../media/icons/${day.Day.Icon}.svg" alt="">
             </div>
             <div>
                 <p>${day.Day.ShortPhrase}</p>
             </div>
             <div>
-                <p>max-temp ${day.Temperature.Maximum.Value}&deg;C<p>
+                <p>${day.Day.Snow.Value}cm Snow</p>
             </div>
+            <div>
+                <p>Temp ${day.Temperature.Minimum.Value} - ${day.Temperature.Maximum.Value}&deg;C<p>
+            </div>
+
         `;
-            days = days.slice(1);
-        });
-
-        icons = document.querySelectorAll('.icon img');
-
-        populateDays(days);
-
-        icons.forEach(icon => {
-            day = days[0];
-            icon.setAttribute('src', `../../../media/icons/${day.Day.Icon}.svg`);
             days = days.slice(1);
         });
     };
