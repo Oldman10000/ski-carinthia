05371e30b5adbad6019dba6090c6ae4a2d5a38ba
Oldman10000 <patrickoldman1993@gmail.com>
2021-07-14 14:11:39 +0000

    Add anchor tag to home page map popups and move js to own file

--- home/static/home/js/home.js
+++ home/static/home/js/home.js
@@ -51,3 +51,42 @@ $(document).ready(function () {
         return false;
     });
 });
+
+let mapTileLayers = L.tileLayer(
+    "http://services.arcgisonline.com/arcgis/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}", {
+        attribution: "Powered by <a href='https://developers.arcgis.com/terms/attribution/' target='_blank' rel='noopener'>Esri</a>"
+    });
+let map = L.map("index-map", {
+    layers: [mapTileLayers],
+    center: [46.787590850347605, 13.87062293485363],
+    minZoom: 9,
+}).setView([46.79, 13.95], 9);
+
+map.setMaxBounds([
+    [47.24078888513102, 11.585583235700277],
+    [46.14847360151397, 16.633618011003204]
+]);
+let skiIcon = new L.Icon({
+    iconUrl: "../../../media/ski-icon2.png",
+    iconSize: [25, 25],
+    iconAnchor: [10, 25]
+});
+
+resorts.forEach(resort => {
+
+    let xReference = resort.fields.x_map_reference;
+    let yReference = resort.fields.y_map_reference;
+
+    let url = "{% url 'resort_detail' 99998888 %}".replace("99998888", resort.pk);
+
+    let marker = L.marker([xReference, yReference], {
+        icon: skiIcon
+    }).addTo(map).bindPopup(`
+            <h6><a href="${url}">${resort.fields.name}</a></h6>
+            <p class="m-0">${resort.fields.street_address_1}</p>
+            <p class="m-0">${resort.fields.street_address_2}</p>
+            <p class="m-0">${resort.fields.postcode} ${resort.fields.town_or_city}</p>
+            <p class="mt-0">${resort.fields.phone_number}</p>
+            `);
+
+});
