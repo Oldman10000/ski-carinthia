122413aed753fc016d78b4d92ded263603ad279e
Oldman10000 <patrickoldman1993@gmail.com>
2021-07-15 10:08:22 +0000

    Change map tiles

--- resorts/static/resorts/js/map.js
+++ resorts/static/resorts/js/map.js
@@ -1,15 +1,22 @@
-let mapTileLayers = L.tileLayer(
-    "http://services.arcgisonline.com/arcgis/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}", {
-        attribution: "Powered by <a href='https://developers.arcgis.com/terms/attribution/' target='_blank' rel='noopener'>Esri</a>"
-    });
+let BasemapAT_basemap = L.tileLayer('https://maps{s}.wien.gv.at/basemap/geolandbasemap/{type}/google3857/{z}/{y}/{x}.{format}', {
+	attribution: 'Datenquelle: <a href="https://www.basemap.at">basemap.at</a>',
+	subdomains: ["", "1", "2", "3", "4"],
+	type: 'normal',
+	format: 'png',
+});
 
-const map = L.map("resort-map", {
-    layers: [mapTileLayers],
+let map = L.map("resort-map", {
+    layers: [BasemapAT_basemap],
     center: [lat, lon],
-    zoom: 2
-}).setView([lat, lon], 11);
+    minZoom: 9,
+}).setView([lat, lon], 14);
 
-const skiIcon = new L.Icon({
+map.setMaxBounds([
+    [47.24078888513102, 11.585583235700277],
+    [46.14847360151397, 16.633618011003204]
+]);
+
+let skiIcon = new L.Icon({
     iconUrl: "../../../media/ski-icon2.png",
     iconSize: [25, 25],
     iconAnchor: [10, 25]
