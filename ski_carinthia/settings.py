ec3bb9c476d01f7dc7e85e5aec33f3d5fb617a11
Oldman10000 <patrickoldman1993@gmail.com>
2021-07-16 09:55:09 +0000

    Remove secret key and set debug

--- ski_carinthia/settings.py
+++ ski_carinthia/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)59wwf1r(ov&$-j07k3h94ab(3*m##df#3so4)!(6#r+l-nf7l'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['ski-carinthia.herokuapp.com', 'localhost']
 
