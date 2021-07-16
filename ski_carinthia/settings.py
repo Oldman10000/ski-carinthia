9f5f6e231220bf58609544bf21d24831842a20d5
Oldman10000 <patrickoldman1993@gmail.com>
2021-07-16 10:38:41 +0000

    Add aws settings

--- ski_carinthia/settings.py
+++ ski_carinthia/settings.py
@@ -51,6 +51,7 @@ INSTALLED_APPS = [
     'blog',
 
     'crispy_forms'
+    'storages',
 ]
 
 MIDDLEWARE = [
@@ -176,6 +177,20 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+if 'USE_AWS' in os.environ:
+    AWS_STORAGE_BUCKET_NAME = 'ski-carinthia'
+    AWS_S3_REGION_NAME = 'eu-west-2'
+    AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com
+
+    # media files
+    DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
+    MEDIAFILES_LOCATION = 'media'
+
+    # override media urls in production
+    MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
