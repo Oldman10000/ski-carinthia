be9dd233a285f030a8add12b504f89baaeb73910
Oldman10000 <patrickoldman1993@gmail.com>
2021-07-16 10:40:27 +0000

    fix typo on aws_s3_custom_domain

--- ski_carinthia/settings.py
+++ ski_carinthia/settings.py
@@ -183,7 +183,7 @@ if 'USE_AWS' in os.environ:
     AWS_S3_REGION_NAME = 'eu-west-2'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
-    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com
+    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 
     # media files
     DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
