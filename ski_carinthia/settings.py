02f48b483ea02f44413c754d7c578235e4c5a19f
Oldman10000 <patrickoldman1993@gmail.com>
2021-07-16 13:25:13 +0000

    Fix bug where media files were not loading correctly

--- ski_carinthia/settings.py
+++ ski_carinthia/settings.py
@@ -171,7 +171,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+if 'DEVELOPMENT' not in os.environ:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
